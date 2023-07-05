93b233327cbe0a05f6cdbd9a0bdaded6f1f6a50a
kayrocesar <kayro.cesar.kc@gmail.com>
2023-07-04 22:41:15 -0300

    fix

--- BEE2617.sql
+++ BEE2617.sql
@@ -32,18 +32,18 @@ create table products(
 -- inserindo nas tabelas 
 
 insert into products (id,name,amount,price,id_providers) values
-	(1,	'Blue Chair',30,300.00,5),
-	(2,	'Red Chair',50,2150.00,1),
+	(1,'Blue Chair',30,300.00,5),
+	(2,'Red Chair',50,2150.00,1),
 	(3,'Disney Wardrobe',400,829.50,4),
-	(4,	'Blue Toaster',20,9.90,3),
+	(4,'Blue Toaster',20,9.90,3),
 	(5,'Solar Panel',30,3000.25,4);
     
 insert into providers (id,name,street,city,state) values 
-	(1,	'Ajax SA','Presidente Castelo Branco','Porto Alegre','RS'),
-	(2,	'Sansul SA','Av Brasil','Rio de Janeiro','RJ'),
-	(3,	'South Chairs',	'Av Moinho', 'Santa Maria','RS'),
-	(4,	'Elon Electro','Apolo','São Paulo','SP'),
-	(5,	'Mike Electro',	'Pedro da Cunha','Curitiba','PR');
+	(1,'Ajax SA','Presidente Castelo Branco','Porto Alegre','RS'),
+	(2,'Sansul SA','Av Brasil','Rio de Janeiro','RJ'),
+	(3,'South Chairs','Av Moinho','Santa Maria','RS'),
+	(4,'Elon Electro','Apolo','São Paulo','SP'),
+	(5,'Mike Electro','Pedro da Cunha','Curitiba','PR');
     
 -- Realizando consulta conforme enunciado 
 
